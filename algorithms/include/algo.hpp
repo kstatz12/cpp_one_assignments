2a82cf149512f90028dc0bbc8d7666153b5f176a
DracoAndTorch <dracoandtorch@gmail.com>
2025-03-09 16:48:34 -0500

    New

--- algorithms/include/algo.hpp
+++ algorithms/include/algo.hpp
@@ -5,17 +5,43 @@
 
 long fibonacci(const int n)
 {
-    return 0;
+    int num1 = 0;
+    int num2 = 1;
+    int num3 = 2;
+    for(int i = 0; i < n; i++) 
+    {
+        num3 = num1 + num2;
+        num1 = num2;
+        num2 = num3;
+    }
+    return num3;
 }
 
 int linear_search(Array * a, const int target)
 {
-    return 0;
+    int idx = 0;
+    int curr = 0;
+    while (curr != target || idx != a->len)
+    {
+        curr = a->data[idx];
+        idx++;
+    }
+    if (curr = target) 
+    {
+        return idx - 1;
+    }
+    return -1;
+
 }
 
 long factorial(const int n)
 {
-    return 0;
+    int sum = 0;
+    for (int i = n; i > 0; i--) 
+    {
+        sum = sum * n;
+    }
+    return sum;
 }
 
 #endif // ALGO_H_
