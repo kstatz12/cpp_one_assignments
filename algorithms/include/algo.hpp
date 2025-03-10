a987bfeaff9a8bb57da2e8911f4ffbe50357d779
snead <snead@colum.edu>
2025-03-10 11:52:27 -0500

    fibonacci function

--- algorithms/include/algo.hpp
+++ algorithms/include/algo.hpp
@@ -5,7 +5,22 @@
 
 long fibonacci(const int n)
 {
-    return 0;
+    int term_1 = 0, term_2 = 1;
+    int nth_term = 0;
+
+    if (n == 1) {
+        return term_1;
+    }
+    if (n == 2) {
+        return term_2;
+    }
+
+    for (int i = 3; i <= n; i++) {
+        nth_term = term_1 + term_2;
+        term_1 = term_2;
+        term_2 = nth_term;
+    }
+    return nth_term;
 }
 
 int linear_search(Array * a, const int target)
