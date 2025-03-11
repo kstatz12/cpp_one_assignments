2abf540fb26583042c6f2f99df2987dca511b76c
snead <snead@colum.edu>
2025-03-11 08:13:40 -0500

    Completed assignment

--- algorithms/include/algo.hpp
+++ algorithms/include/algo.hpp
@@ -2,40 +2,40 @@
 #define ALGO_H_
 #include "types.hpp"
 
-inline long fibonacci(const int n) {
-    int term_0 = 0, term_1 = 1;
-    int nth_term = 0;
+inline long fibonacci(const int n)
+{
+    int nth_term = 0, nth_minus_1 = 1, nth_minus_2 = 0;
 
-    if (n == 0) {
-        return term_0;
-    }
+    if (n == 0) {return 0;}
+    if (n == 1) {return 1;}
 
-    if (n == 1) {
-        return term_1;
-    }
-
-    for (int i = 2; i <= n; i++) {
-        nth_term = term_0 + term_1;
-        term_0 = term_1;
-        term_1 = nth_term;
+    for (int i = 2; i <= n; i++)
+    {
+        nth_term = nth_minus_2 + nth_minus_1;
+        nth_minus_2 = nth_minus_1;
+        nth_minus_1 = nth_term;
     }
 
     return nth_term;
 }
 
-inline int linear_search(Array const * a, const int target) {
-    for (int i = 0; i < a->len; i++) {
-        if (a->data[i] == target) {
+inline long linear_search(Array const *a, const int target)
+{
+    for (unsigned int i = 0; i < a->len; i++)
+    {
+        if (a->data[i] == target)
+        {
             return i;
-            }
         }
+    }
+
     return -1;
 }
 
-
 inline long factorial(const int n)
 {
-    int nth_term = 1; //0! = 1
+    int nth_term = 1; // 0! = 1
+
     for (int i = 1; i <= n; i++)
     {
         nth_term *= i;
@@ -43,6 +43,5 @@ inline long factorial(const int n)
 
     return nth_term;
 }
-
 
 #endif // ALGO_H_
